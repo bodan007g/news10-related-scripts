24b302e9c67b2128bcc4f77e7f3fff480609378f
Daniel B <bodan10@yahoo.com>
2025-08-23 10:30:00 +0300

    Fix activation of virtual environment in webhook server startup script

--- start_webhook_server.sh
+++ start_webhook_server.sh
@@ -2,4 +2,5 @@
 # Start the multi-project webhook server in production mode
 export SERVER_ENV=production
 VHOSTS_BASE="/var/www/vhosts"
+source "$VHOSTS_BASE/news10-related-scripts/venv/bin/activate"
 python3 "$VHOSTS_BASE/news10-related-scripts/webhook_server.py"
